d67c63577040f2fced9cb163dc6da07513f27934
AmirAliAzimloo <amiraliazimloo123@gmail.com>
2024-05-31 14:43:09 -0700

    Add the StackNavigation and the DeepLinking logic.

--- App.tsx
+++ App.tsx
@@ -1,8 +1,85 @@
 import React, { useEffect } from 'react'
-import { PermissionsAndroid, StyleSheet, Text, View } from 'react-native'
+import { ActivityIndicator, Linking, PermissionsAndroid, StyleSheet} from 'react-native'
 import messaging from '@react-native-firebase/messaging';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+
+import Home from './components/Home';
+import Setting from './components/Setting';
+
+
+//* constants
+const Stack = createStackNavigator();
+const NAVIGATION_IDS = ["home", "settings"];
+
+function buildDeepLinkFromNotificationData(data:any): string | null {
+  const navigationId = data?.navigationId;
+  if (!NAVIGATION_IDS.includes(navigationId)) {
+    console.warn('Unverified navigationId', navigationId)
+    return null;
+  }
+  if (navigationId === "home") {
+    return 'myapp://home';
+  }
+  if (navigationId === "settings") {
+    return 'myapp://settings';
+  }
+  
+  return null
+}
+
+const linking = {
+  prefixes: ['myapp://'],
+  config: {
+    screens: {
+      Home: "home",
+      Settings: "settings"
+    }
+  },
+  async getInitialURL() {
+    const url = await Linking.getInitialURL();
+    if (typeof url === 'string') {
+      return url;
+    }
+    //getInitialNotification: When the application is opened from a quit state.
+    const message = await messaging().getInitialNotification();
+    const deeplinkURL = buildDeepLinkFromNotificationData(message?.data);
+    if (typeof deeplinkURL === 'string') {
+      return deeplinkURL;
+    }
+  },
+  subscribe(listener: (url: string) => void) {
+    const onReceiveURL = ({url}: {url: string}) => listener(url);
+
+    // Listen to incoming links from deep linking
+    const linkingSubscription = Linking.addEventListener('url', onReceiveURL);
+    messaging().setBackgroundMessageHandler(async remoteMessage => {
+      console.log('Message handled in the background!', remoteMessage);
+    });
+
+    const foreground = messaging().onMessage(async remoteMessage => {
+      console.log('A new FCM message arrived!', remoteMessage);
+
+    });
+    //onNotificationOpenedApp: When the application is running, but in the background.
+    const unsubscribe = messaging().onNotificationOpenedApp(remoteMessage => {
+      const url = buildDeepLinkFromNotificationData(remoteMessage.data)
+      if (typeof url === 'string') {
+        listener(url)
+      }
+    });
+
+    return () => {
+      linkingSubscription.remove();
+      unsubscribe();
+      foreground();
+    };
+  },
+}
 
 const App = () => {
+
 
   //* get notification permision
   useEffect(()=>{
@@ -24,9 +101,12 @@ const App = () => {
    },[])
 
   return (
-    <View>
-      <Text>Hello world !</Text>
-    </View>
+    <NavigationContainer linking={linking} fallback={<ActivityIndicator animating />}>
+    <Stack.Navigator initialRouteName='Home'>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Settings" component={Setting} />
+    </Stack.Navigator>
+  </NavigationContainer>
   )
 }
 
